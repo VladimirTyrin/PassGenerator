d9542bfa5d28099b69e795af963482b39f53e77f
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-03-14 17:15:34 +0300

    Implementation added

--- PassGenerator/Program.cs
+++ PassGenerator/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PassGenerator
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private const int PasswordLength = 12;
+        private static readonly Random Random = new Random();
+
+        private static T PickRandom<T>(this IReadOnlyList<T> list) => list[Random.Next(list.Count)];
+
+        private static void Main(string[] args)
         {
+            var chars = new List<char>();
+            for (var i = 'a'; i < 'z'; ++i)
+                chars.Add(i);
+            for (var i = 'A'; i < 'Z'; ++i)
+                chars.Add(i);
+            for (var i = '0'; i < '9'; ++i)
+                chars.Add(i);
+
+            var passBuilder = new StringBuilder();
+            for (var i = 0; i < PasswordLength; i++)
+            {
+                passBuilder.Append(chars.PickRandom());
+            }
+
+            var pass = passBuilder.ToString();
+            Console.WriteLine(pass);
         }
     }
 }
