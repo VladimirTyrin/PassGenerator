b15efe776d202f31f319e6ef37336d9410dcf3eb
Vladimir Tyrin <tyrin@mindbox.cloud>
2023-07-20 17:10:09 +0600

    Time for update

--- PassGenerator/Program.cs
+++ PassGenerator/Program.cs
@@ -1,34 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
-namespace PassGenerator
+const int passwordLength = 12;
+var random = new Random();
+
+var chars = new List<char>();
+for (var i = 'a'; i <= 'z'; ++i)
+    chars.Add(i);
+for (var i = 'A'; i <= 'Z'; ++i)
+    chars.Add(i);
+for (var i = '0'; i <= '9'; ++i)
+    chars.Add(i);
+
+var passBuilder = new StringBuilder();
+for (var i = 0; i < passwordLength; i++)
 {
-    internal static class Program
-    {
-        private const int PasswordLength = 12;
-        private static readonly Random Random = new Random();
-
-        private static T PickRandom<T>(this IReadOnlyList<T> list) => list[Random.Next(list.Count)];
-
-        private static void Main()
-        {
-            var chars = new List<char>();
-            for (var i = 'a'; i <= 'z'; ++i)
-                chars.Add(i);
-            for (var i = 'A'; i <= 'Z'; ++i)
-                chars.Add(i);
-            for (var i = '0'; i <= '9'; ++i)
-                chars.Add(i);
-
-            var passBuilder = new StringBuilder();
-            for (var i = 0; i < PasswordLength; i++)
-            {
-                passBuilder.Append(chars.PickRandom());
-            }
-
-            var pass = passBuilder.ToString();
-            Console.WriteLine(pass);
-        }
-    }
+    passBuilder.Append(chars[random.Next(chars.Count)]);
 }
